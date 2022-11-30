8208e56828d1e3fbaf463d4569ef27e8c2d05400
chiucpw <chiujack5354@gmail.com>
2022-11-30 22:13:17 +0800

    HW5

---
+++ p06.c
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+int cubeByvalue(int n);
+
+int main()
+{
+	int number = 5;
+	printf("The original value of number is %d", number);
+	number = cubeByvalue(number);
+	printf("\nThe new value of number is %d\n", number);
+}
+
+int cubeByvalue(int n)
+{
+	return n * n * n;
+}
